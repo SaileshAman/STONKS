b3b447a17d8628986b739adb9b6d06dbbaf09a32
ficklemind09 <saileshaman.dola2020@vitstudent.ac.in>
2021-06-20 17:13:32 +0530

    2nd Commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Client)
+admin.site.register(Broker)
+admin.site.register(Stock)
+admin.site.register(Client_Stock)
+admin.site.register(Client_Broker)
--- accounts/models.py
+++ accounts/models.py
@@ -2,3 +2,50 @@ from django.db import models
 
 # Create your models here.
 
+class Client(models.Model):
+    CID = models.CharField(max_length = 12)
+    Name = models.CharField(max_length = 50)
+    Email = models.EmailField()
+    Portfolio_ID = models.CharField(max_length = 15)
+    PAN = models.CharField(max_length = 14)
+    data_created = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.CID} {self.Name}"
+
+class Broker(models.Model):
+    BID = models.CharField(max_length = 12)
+    Name = models.CharField(max_length = 50)
+    Email = models.EmailField(null = True)
+    License = models.CharField(max_length = 14)
+    data_created  = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.BID} {self.Name}"
+
+class Stock(models.Model):
+    SID = models.CharField(max_length=12)
+    Name = models.CharField(max_length = 50)
+    Price = models.FloatField()
+    Company = models.CharField(max_length=50)
+    data_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.SID} {self.Name}"
+
+class Client_Stock(models.Model):
+    CID = models.ForeignKey(Client, on_delete = models.CASCADE)
+    SID = models.ForeignKey(Stock, on_delete = models.CASCADE)
+    Shares = models.FloatField()
+    data_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.CID.Name} ---> {self.SID.Name}"
+
+class Client_Broker(models.Model):
+    PID = models.ForeignKey(Client, on_delete = models.CASCADE)
+    BID = models.ForeignKey(Broker, on_delete = models.CASCADE)
+    data_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.PID.Name} ---> {self.Name}"
